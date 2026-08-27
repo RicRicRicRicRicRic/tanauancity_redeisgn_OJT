ce6751228ba887b898cb5cafa9386177d68e7b42
VinsG <vinsgarniel@gmail.com>
2026-08-27 16:27:05 +0800

    Basta update, nag bura lang e

--- tanauan/src/components/government_tab/mission_and_vision.tsx
+++ tanauan/src/components/government_tab/mission_and_vision.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Footer from '../layout/Footer';
 import mision_and_vision_bg from '../../assets/sections/pictures/Tnauan.png';
 
